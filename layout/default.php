ff81b0c64a2c7d9402a12346c7849ccec06502b0
HAL\Dominik <dominik.tomcik23@gmail.com>
2018-03-10 21:47:05 +0100

    intro

--- layout/default.php
+++ layout/default.php
@@ -1,64 +1,106 @@
 		<div class="content">
 		<div class="intro">
-			
+			<h1>Zkoumáme formu, dáváme věcem a strojům tvar a smysl. Chceme zbavit slovo design významu pouhé vizuální stylizace. Hledáme odpovědi na otázky doby. Snažíme se definovat současnost a vědět jak bude vypadat budoucnost.</h1>
+			<a class="btn" href="#grid">Projekty</a>
+			<a class="btn" href="#about">Spolupráce</a>
 		</div>
-		<h1>Projekty</h1>
-		<div class="grid">
+		<div id="grid" class="grid">
 			<div class="item01">		
-					<h2>Cognitive Hub<br />Konica Minolta</h2>		
 					<img src="img/landingpage/server.jpg" />
+					<div class="envelope">
+						<h2>Cognitive Hub<br />Konica Minolta</h2>			
+						<p>realizace</p>
+					</div>
 					<img id="id01" class="remove" src="img/landingpage/server_col.jpg" />
 			</div>
 
 			<div class="item02">					
 					<img src="img/landingpage/generator.jpg" />
+					<div class="envelope">
+						<h2>Gentec Kogenerační jednotka</h2>
+						<p>realizace</p>
+					</div>
 					<img id="id02" class="remove" src="img/landingpage/generator_col.jpg" />
 			</div>
 
 			<div class="item03">					
 					<img src="img/landingpage/kombajn.jpg" />
-					<h2>autonomní<br />zemědělský kombajn</h2>
+					<div class="envelope">
+						<h2>autonomní zemědělský kombajn</h2>
+						<p>koncept</p>
+					</div>
 					<img id="id03" class="remove" src="img/landingpage/kombajn_col.jpg" />
 			</div>
 
 			<div class="item04">					
 					<img src="img/landingpage/vyhledavac.jpg" />
+					<div class="envelope">
+						<h2>Lavinový vyhledávač</h2>
+						<p>koncept</p>
+					</div>
 					<img id="id04" class="remove" src="img/landingpage/vyhledavac_col.jpg" />
 			</div>
 
 			<div class="item05">				
 					<img src="img/landingpage/lavicka.jpg" />
+					<div class="envelope">
+						<h2>Prvky městského mobiliáře</h2>
+						<p>koncept</p>
+					</div>
 					<img id="id05" class="remove" src="img/landingpage/lavicka_col.jpg" />
 			</div>
 
-			<div class="item06">
-					<h2>Traktor<br /> na elektrický pohon</h2>				
+			<div class="item06">				
 					<img src="img/landingpage/traktor.jpg" />
+					<div class="envelope">
+						<h2>Traktor na elektrický pohon</h2>
+						<p>koncept</p>
+					</div>
 					<img id="id06" class="remove" src="img/landingpage/traktor_col.jpg" />
 			</div>
 
 			<div class="item07">				
 					<img src="img/landingpage/electrolux.jpg" />
+					<div class="envelope">
+						<h2>Čistička vzduchu Electrolux Laboratory</h2>
+						<p>koncept</p>
+					</div>
 					<img id="id07" class="remove" src="img/landingpage/electrolux_col.jpg" />
 			</div>
 
 			<div class="item08">				
 					<img src="img/landingpage/pila.jpg" />
+					<div class="envelope">
+						<h2>Pásová pila</h2>
+						<p>koncept</p>
+					</div>
 					<img id="id08" class="remove" src="img/landingpage/pila_col.jpg" />
 			</div>
 
 			<div class="item09">				
 					<img src="img/landingpage/tramvaj.jpg" />
+					<div class="envelope">
+						<h2>Autonomní tramvaj</h2>
+						<p>koncept</p>
+					</div>
 					<img id="id09" class="remove" src="img/landingpage/tramvaj_col.jpg" />
 			</div>
 
 			<div class="item10">				
 					<img src="img/landingpage/pece.jpg" />
+					<div class="envelope">
+						<h2>Pekařské pece Kornfeil</h2>
+						<p>realizace</p>
+					</div>
 					<img id="id10" class="remove" src="img/landingpage/pece_col.jpg" />
 			</div>
 
 			<div class="item11">				
 					<img src="img/landingpage/auto.jpg" />
+					<div class="envelope">
+						<h2>Skici</h2>
+						<p>koncept</p>
+					</div>
 					<img id="id11" class="remove" src="img/landingpage/auto_col.jpg" />
 			</div>
 		</div>
